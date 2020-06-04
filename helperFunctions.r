5051e8cf86411303edbddd04ae6f60d11f6a0113
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-06-04 08:08:51 -0700

    removed cropData from helper functions

--- helperFunctions.r
+++ helperFunctions.r
@@ -14,21 +14,3 @@ gm_mean = function(x, na.rm=TRUE){
 }
 
 
-
-# Crop data: Filter to only include years with data in all years between startYr and endYr for MU =======================
-
-cropData<-function(Dat,startEndYrs, MU_ID) {
-  # Extract start and end years for that MU  
-  startYr<-startEndYrs[as.character(startEndYrs$MU_ID)==MU_ID,"LRPStartYr"]
-  endYr<-startEndYrs[as.character(startEndYrs$MU_ID)==MU_ID,"LRPEndYr"]
-  
-  # Filter to only include years with data in all years between startYr and endYr for MU
-  Dat.MU <- Dat%>% filter(MU == MU_ID & yr >= startYr & yr <= endYr)
-  required_nYrs<-length(startYr:endYr)
-  nYrs_byCU <- Dat.MU %>% group_by(CU)  %>% summarize(nYrs = length(na.omit(ets))) 
-  
-  Dat.MU<-left_join(Dat.MU, nYrs_byCU,by = "CU") %>% filter(nYrs == required_nYrs)
-  
-  Dat.MU
- 
-}

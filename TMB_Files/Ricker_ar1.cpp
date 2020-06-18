038a5045581ef7ac0223c93f029de9879f0b6f4b
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-06-18 14:14:10 -0700

    added SMSY to RickerAR1.cpp

--- TMB_Files/Ricker_ar1.cpp
+++ TMB_Files/Ricker_ar1.cpp
@@ -1,5 +1,45 @@
 #include <TMB.hpp >
 
+// Set up Lambert's W function to use to calculate SMSY
+// Code taken from https://kaskr.github.io/adcomp/lambert_8cpp_source.html
+// Step 1: Code up a plain C version
+// Double version of Lambert W function
+double LambertW(double x) {
+  double logx = log(x);
+  double y = (logx > 0 ? logx : 0);
+  int niter = 100, i=0;
+  for (; i < niter; i++) {
+    if ( fabs( logx - log(y) - y) < 1e-9) break;
+    y -= (y - exp(logx - y)) / (1 + y);
+  }
+  if (i == niter) Rf_warning("W: failed convergence");
+  return y;
+}
+
+TMB_ATOMIC_VECTOR_FUNCTION(
+  // ATOMIC_NAME
+  LambertW
+  ,
+  // OUTPUT_DIM
+  1,
+  // ATOMIC_DOUBLE
+  ty[0] = LambertW(tx[0]); // Call the 'double' version
+,
+// ATOMIC_REVERSE
+Type W  = ty[0];                    // Function value from forward pass
+Type DW = 1. / (exp(W) * (1. + W)); // Derivative
+px[0] = DW * py[0];                 // Reverse mode chain rule
+)
+  
+  // Scalar version
+  template<class Type>
+  Type LambertW(Type x){
+    CppAD::vector<Type> tx(1);
+    tx[0] = x;
+    return LambertW(tx)[0];
+  }
+  
+  
 template<class Type>
 Type objective_function<Type>:: operator() ()
 {
@@ -25,7 +65,7 @@ Type objective_function<Type>:: operator() ()
   vector <Type> sigma_ar = exp(logSigma_ar);
   vector <Type> A_ar = exp(logA_ar);
   vector <Type> err(N_Obs);
-  
+
   
   // Ricker AR1 likelihood
   for (int i = 0; i<N_Obs; i++){
@@ -55,6 +95,8 @@ Type objective_function<Type>:: operator() ()
   int N_stks = model.size(); 
   vector <Type> logA_(N_stks);
   vector <Type> logB_(N_stks);
+  vector <Type> SMSY(N_stks);  
+  vector <Type> B = exp(logB_);
   
   for (int i = 0; i<N_stks; i++){
     if(model(i) == 0) {
@@ -68,12 +110,21 @@ Type objective_function<Type>:: operator() ()
     
   }
   
-  ADREPORT(A_ar);
-  ADREPORT(A_std);
-  ADREPORT(logA_);
-  ADREPORT(LogR_Pred_ar);
-  ADREPORT(LogR_Pred_std);
   
+  // Calculate SMSY using Lambert's W function
+  // Approach from Scheurell 2016
+
+  
+  for(int i=0; i<N_stks; i++){
+    SMSY[i] =  (1 - LambertW(exp(1-logA_[i])) ) / B[i] ;
+  }
+  
+  //ADREPORT(A_ar);
+  //ADREPORT(A_std);
+  ADREPORT(logA_);
+  //ADREPORT(LogR_Pred_ar);
+  //ADREPORT(LogR_Pred_std);
+  ADREPORT(SMSY);
   return ans;
   
 }

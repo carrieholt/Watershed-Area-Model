7cc50bd88db1de683127609429ba380f6eee6238
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-07-20 15:54:51 -0700

    added CheckAR1.r

---
+++ CheckAR1.r
@@ -0,0 +1,94 @@
+
+#---------------------------------------------------------
+# Libaries
+
+library(rsample)
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+library(TMB)
+library(zoo)
+
+#Functions
+count.dig <- function(x) {floor(log10(x)) + 1}
+'%not in%' <- function (x, table) is.na(match(x, table, nomatch=NA_integer_))
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# Are residuals of Ricker model autocorrelated? Run Ricker_CheckAr1.cpp TMB code below to check
+
+SRDatwNA <- read.csv("DataIn/SRinputfile.csv")
+SRDatwNA <- SRDatwNA %>% filter(Name != "Hoko" & Name != "Hoh") #remove two stocks not used in Parken et al, and not documented in Liermann et al.
+stockwNA <- SRDatwNA %>% filter (is.na(Rec) == TRUE) %>% select (Stocknumber) %>% unique() %>% unlist() #Do not use AR(1) model on 3  stocks with NAs
+
+# Remove years with NAs
+SRDat <- SRDatwNA %>% filter(Rec != "NA") #%>% filter(Stocknumber <= 24)
+
+# Revise yr_num list where NAs have been removed
+test <- SRDat %>% filter(Stocknumber == stockwNA[1]| Stocknumber == stockwNA[2])
+if( max(SRDat$Stocknumber) >= stockwNA[1]) {
+  for (i in 1:length(stockwNA)) {
+    len <- length (SRDat [which (SRDat$Stocknumber == stockwNA[i]), ]$yr_num) - 1
+    SRDat [which (SRDat$Stocknumber == stockwNA[i]), ]$yr_num <- c (0:len)
+  }
+}
+test <- SRDat %>% filter(Stocknumber == stockwNA[1]| Stocknumber == stockwNA[2])
+
+
+# Calculate scale for each stock
+digits <- SRDat %>% group_by(Stocknumber) %>% summarize(maxDigits = count.dig(max(Sp)))
+SRDat <- left_join(SRDat, digits)
+SRDat <- SRDat %>% mutate(Scale = 10^(maxDigits-1))
+
+TMB_Inputs <- list(logA_Start = 2, rho_Start = 0.1, Sgen_sig = 1) #Scale = 1000, 
+
+# Data 
+data <- list()
+Scale <- SRDat$Scale # Scale <- TMB_Inputs$Scale
+data$S <- SRDat$Sp/Scale 
+data$logR <- log(SRDat$Rec/Scale)
+data$stk <- as.numeric(SRDat$Stocknumber)
+N_Stocks <- length(unique(SRDat$Name))
+data$yr <- SRDat$yr_num
+data$model <- rep(0,N_Stocks)
+
+
+# Parameters
+param <- list()
+Scale.stock <- 10^(digits$maxDigits-1)
+param$logA_std <- ( SRDat %>% group_by (Stocknumber) %>% summarise(yi = lm(log( Rec / Sp) ~ Sp )$coef[1] ) )$yi
+B_std <- SRDat %>% group_by(Stocknumber) %>% summarise( m = - lm(log( Rec / Sp) ~ Sp )$coef[2] )
+param$logB_std <- log ( 1/ ( (1/B_std$m)/Scale.stock ))#log(B_std$m/Scale.stock)
+param$logSigma_std <- rep(-2, N_Stocks)
+
+
+# Compile model if changed:
+#dyn.unload(dynlib("TMB_Files/Ricker_CheckAr1"))
+#compile("TMB_Files/Ricker_CheckAr1.cpp")
+
+dyn.load(dynlib("TMB_Files/Ricker_CheckAr1"))
+
+obj <- MakeADFun(data, param, DLL="Ricker_CheckAr1", silent=TRUE)
+
+opt <- nlminb(obj$par, obj$fn, obj$gr, control = list(eval.max = 1e5, iter.max = 1e5))
+pl <- obj$env$parList(opt$par) 
+summary(sdreport(obj))
+
+All_Ests <- data.frame(summary(sdreport(obj)))
+All_Ests$Param <- row.names(All_Ests)
+All_Ests$Param <- sapply(All_Ests$Param, function(x) (unlist(strsplit(x, "[.]"))[[1]]))
+Preds <- All_Ests %>% filter(Param == "LogR_Pred_std")
+Preds <- Preds %>% add_column(yr=data$yr, Stocknumber=data$stk, logR=data$logR) %>% mutate(Resid=Estimate-logR) 
+ac <- Preds %>% group_by(Stocknumber) %>% summarise (autocorr=acf(Resid, plot=F)$acf[2])# provides AR(1) autocorrelation
+len <- Preds %>% group_by(Stocknumber) %>% summarise (count=length(Resid))
+ac.CI <- function(n) {qnorm((1 + 0.95)/2)/sqrt(n)} #95% CI for acf assuming white noise, see https://stackoverflow.com/questions/14266333/extract-confidence-interval-values-from-acf-correlogram
+len <- len %>% mutate (CI=ac.CI(count))
+ac <- ac %>% left_join(len) %>% left_join(unique(SRDat[, c("Stocknumber", "Name")])) %>% filter(abs(autocorr)>CI)
+ac # 6 stocks have significant lag-1 autocorrelation: Chikamin, Keta, Blossom, Situk, Siletz, and Columbia Sp
+
+
+A_std <- All_Ests %>% filter(Param=="logA_std") %>% add_column(Stocknumber=unique(data$stk)) %>% mutate(A=exp(Estimate))
+B_std <- All_Ests %>% filter(Param=="logB_std") %>% add_column(Stocknumber=unique(data$stk)) %>% mutate(B=exp(Estimate)/Scale.stock) 
+SMSY_std <- All_Ests %>% filter(Param=="SMSY") %>% add_column(Stocknumber=unique(data$stk)) 
+
+

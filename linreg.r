3ecae706e451f3d4a3fe21a51c90dc8972dc9087
carrieholt <Carrie.Holt@dfo-mpo.gc.ca>
2020-10-14 15:57:13 -0700

    added sigmR_metaanalysis

--- linreg.r
+++ linreg.r
@@ -21,12 +21,16 @@ param$sd <- 1
 
 
 # Run TMB 
-#compile("linreg.cpp")
-dyn.load(dynlib("linreg"))
+#compile("TMB_Files/linreg.cpp")
+dyn.load(dynlib("TMB_Files/linreg"))
 obj <- MakeADFun(data, param, DLL="linreg")
 opt <- nlminb(obj$par, obj$fn, obj$gr, control = list(eval.max = 1e5, iter.max = 1e5))#lower=as.vector(lower), upper=as.vector(upper))
 pl <- obj$env$parList(opt$par) # Parameter estimate after phase 1
 summary(sdreport(obj), p.value=TRUE)
+
+# Look at example simulated output
+obj$simulate()$y
+
 
 # Simulate 1000 predicted data sets (at x fixed values)
 Y_Preds <- matrix(nrow = 1000, ncol = length(x))
@@ -39,17 +43,6 @@ Y_Pred_Summ <- apply(Y_Preds, 2, quantile, probs = c(0.025, 0.5, 0.975))
 
 Y_Pred_Summ
 
-# simulate from multivariate normal
-sims <- rmvnorm(1000, unlist(pl), solve(obj$he()))
-# now for each simulate estimates
-Preds <- matrix(nrow = 1000, ncol = length(x))
-for(i in 1:dim(sims)[1]){
-  #Preds[i, ] <- rnorm(length(x), sims[i, 1] + sims[i, 2]*x, sims[i, 3])
-  #Preds[i, ] <- sims[i, 1] + sims[i, 2]*x
-  Preds[i, ] <- rnorm(length(x), sims[i, 1] + sims[i, 2]*x, pl$sd)
-}
-apply(Preds, 2, quantile, probs = c(0.025, 0.5, 0.975))
-
 
 # EStiamte prediction intervals using R function predict.lm()
 lmtest <- lm(y~x)
@@ -58,8 +51,6 @@ predict.lm(lmtest, interval="predict")
 
 
 # These prediction intervals are larger than those using obj$simulate()
-
-# Carrie code to calculate prediction intervals that match predict.lm:
 
 # STEYX from https://support.microsoft.com/en-us/office/steyx-function-6ce74b2c-449d-4a6e-b9ac-f9cef5ba48ab
 sumXErr <- sum( (x-mean(x))^2 )
@@ -109,43 +100,3 @@ PredInt <- function(x,y,Newx=x, Predy){
 }
 
 PredInt(x=x,y=y,Predy=predict(lmtest))
-
-
-
-# try with using loga, logb, logsd -- gets even wonkier results
-#compile("linreg_log.cpp")
-dyn.load(dynlib("linreg_log"))
-
-data$JA = 0
-param <- list()
-param$loga <- 0
-param$logb <- 0
-param$logsd <- 0
-
-
-obj_log <- MakeADFun(data, param, DLL="linreg_log", hessian = T)
-opt_log <- nlminb(obj_log$par, obj_log$fn, obj_log$gr, 
-                  control = list(eval.max = 1e5, iter.max = 1e5))    #lower=as.vector(lower), upper=as.vector(upper))
-pl_log <- obj_log$env$parList(opt_log$par) # Parameter estimate after phase 1
-summary(sdreport(obj_log), p.value=TRUE)
-
-Y_Preds_log <- matrix(nrow = 1000, ncol = length(x))
-for(i in 1:1000){
-  Y_Preds_log[i, ] <- obj_log$simulate()$y
-}
-apply(Y_Preds_log, 2, quantile, probs = c(0.025, 0.5, 0.975))
-
-# what if simulate combinations using mvnorm 
-library(mvtnorm)
-# getting negative values when do this -- should have logA, logB, logsd as params
-sims <- rmvnorm(1000, exp(unlist(pl_log)), solve(obj_log$he()))
-# now for each simulate estimates
-Preds <- matrix(nrow = 1000, ncol = length(x))
-for(i in 1:dim(sims)[1]){
-  #Preds[i, ] <- exp(sims[i, 1]) + exp(sims[i, 2])*x
-  #Preds[i, ] <- rnorm(length(x), exp(sims[i, 1]) + exp(sims[i, 2])*x, exp(sims[i, 3]))
-  Preds[i, ] <- sims[i, 1] + sims[i, 2]*x
-}
-apply(Preds, 2, quantile, probs = c(0.025, 0.5, 0.975))
-
-# nope that didn't work!
---
+++ sigR_metaanalysis.R
@@ -0,0 +1,17 @@
+#Code to estimate sigR from PSE data
+
+#---------------------------------------------------------
+# Libaries
+
+library(rsample)
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+library(TMB)
+library(zoo)
+library(viridis)
+library(hrbrthemes)
+
+
+SRDatwNA <- read.csv("DataIn/PSE_ChinookData.csv")

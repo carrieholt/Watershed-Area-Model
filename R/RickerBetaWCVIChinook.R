b3a5343b4c1356a416944e26a71d60a5671f97e0
Holt <Carrie.Holt@dfo-mpo.gc.ca>
2024-12-10 13:13:35 -0800

    added updated RickerBetaWCVIChinook calcs for set of indicators used HoltK et al. 2023

--- R/RickerBetaWCVIChinook.R
+++ R/RickerBetaWCVIChinook.R
@@ -55,3 +55,39 @@ out
 #  4802          Kyuquot         1.576 0.0003281966
 #  1166 Nootka/Esperanza         1.576 0.0013516295
 #  3155         Quatsino         1.433 0.0004541997
+
+# For only indicators with PNI>0.5 as in Holt, K. et al 2013
+
+remove.EnhStocks <- TRUE
+
+cus <- c("WCVI_Nootka_Kyuquot","WCVI_North", "WCVI_South")
+# lnalpha_cu <- c( 1.576, 1.433, 0.815)
+lnalpha_cu <- c(1.578412, 1.529675, 1.138784)
+
+# inlets <- c("Kyuquot", "Clayoquot", "Quatsino", "Barkley", "Nootka/Esperanza")
+# lnalpha_inlet <- c(1.576, 0.815, 1.433, 0.815, 1.576)
+inlets <- c("Kyuquot", "Clayoquot", "Quatsino", "Barkley", "Nootka/Esperanza")
+lnalpha_inlet_wbc <- c(1.578412, 1.138784, 1.529675, 1.138784, 1.578412)
+lnalpha_inlet_nbc <- c(1.343629, 0.817727, 1.297002, 0.817727, 1.343629)
+# See DataIn/riclogArr.csv and DataIn/riclogArr_nBC.csv in SalmonLRP_WCVI_CK 
+# repository, created in R file, runWCVIChinook_projLRP.R, section 13 with bias 
+# correction on or off, respectively.
+
+dum <- data.frame(inlets=inlets, lnalpha_inlet_wbc=lnalpha_inlet_wbc, 
+                  lnalpha_inlet_nbc=lnalpha_inlet_nbc)
+
+
+# Read in SREP values from watershed-area model
+# if(remove.EnhStocks) wcviRPs_long <- read.csv("DataOut/WCVI_SMSY_noEnh.csv")
+wcviRPs_long <- read.csv("DataOut/WCVI_SMSY_noEnh_wBC.csv")
+
+dum2 <- wcviRPs_long %>%  filter (Param == "SREP") %>% 
+  filter(Stock %in% inlets) %>% rename(inlets=Stock, SREP=Estimate) %>% select(-c(LL, UL, Param, X))
+# Remove double instance of San Juan, as it's a indicator stock and an inlet
+# dum2 <- dum2[-1,]
+# dum2$SREP <- signif(dum2$SREP,2)
+
+out <- dum2 %>% left_join(dum, by="inlets")
+out <- out %>% mutate(beta=lnalpha_inlet_nbc/SREP)
+out
+
